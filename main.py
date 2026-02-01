0f4cc400c7ce093709e3e407511cac7a17287b36
kasanenkooleksandr031-del <kasanenkooleksandr031@gmail.com>
2026-02-01 14:12:25 +0200

    created function that append elements in lists

--- main.py
+++ main.py
@@ -1,1 +1,10 @@
-print("Hello World!")
+my_list = []
+
+def my_func (a, b, c):
+    my_list.append(a)
+    my_list.append(b)
+    my_list.append(c)
+
+my_func(input("ведіть 1 елемент списку - "), input("ведіть 2 елемент списку - "), input("ведіть 3 елемент списку - "))
+
+print(my_list)
